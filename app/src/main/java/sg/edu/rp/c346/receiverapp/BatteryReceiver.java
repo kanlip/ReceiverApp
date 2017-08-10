3e9cba10b434c058ae8b21433da7ecae862a32b8
LIPIPAN SUKPHASUTH <kan_lip@hotmail.com>
2017-08-10 10:09:15 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/receiverapp/BatteryReceiver.java
@@ -0,0 +1,20 @@
+package sg.edu.rp.c346.receiverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by 15015860 on 8/10/2017.
+ */
+
+public class BatteryReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast my_toast = Toast.makeText(context,"Low Battery",Toast.LENGTH_LONG);
+        my_toast.show();
+    }
+}
